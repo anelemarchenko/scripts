2bc667cdf2398c444eb2f80eb8451395f2b09ce7
Olena Marchenko <olena.marchenko@symphonyteleca.com>
2015-01-22 15:34:34 +0100

    works! But need to create folder

--- bookdownloads-in-proc.sh
+++ bookdownloads-in-proc.sh
@@ -5,24 +5,19 @@ fail_attempt=0					        #fails counter
 base_url_ebook='http://it-ebooks.info/book/'		#setting up the base part of book's url
 while							#begining of loop
  [  $fail_attempt -lt 10 ]; do
-echo -------
-echo "---> Next download has started"
-echo "Book number" $page
+echo "--> Book number" $page
 url_ebook="$base_url_ebook$page/"
-echo "ebook link (url_ebook)" $url_ebook
+echo "url_ebook=" $url_ebook
+url_for_download=$(curl $url_ebook | grep -o "http:\/\/filepi.*'" | rev | cut -c 2- | rev)
+echo "FIrst curl works? url_for_download=" $url_for_download
 echo -n "Continue? [y/n]: "
 read
-if [ "$REPLY" = "y" ]; then				# debug pause
-echo "CONTINUE"
+if [ "$REPLY" = "y" ]; then                             # debug pause
+echo
 else break    
 fi
-echo -------
-echo "сейчас будет проверка работы curl с параметром url_ebook"
-curl $url_ebook
-
-url_for_download=$(curl "$url_ebook" | grep -o "http:\/\/filepi.*'" | rev | cut -c 2- | rev)
-echo "curl works? url_for_download=" $url_for_download
-real_url=$(curl $url_for_download -I -H 'Referer: $url_ebook' | grep -o "http[a-zA-Z0-9\/:.]*")
+real_url=$(curl $url_for_download -I -H  'Referer:'$url_ebook | grep -o "http[a-zA-Z0-9\/:.]*")
+echo "2curl works? url_for_download=" $real_url
 echo
 wget --content-disposition  $real_url
 let page=page+1
