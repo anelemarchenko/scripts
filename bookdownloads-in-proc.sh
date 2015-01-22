e5ce0ca12eadd714af68bdab3d143aadeb90b17b
Olena Marchenko <olena.marchenko@symphonyteleca.com>
2015-01-22 16:00:17 +0100

    It looks like everythin is working

--- bookdownloads-in-proc.sh
+++ bookdownloads-in-proc.sh
@@ -9,17 +9,17 @@ echo "--> Book number" $page
 url_ebook="$base_url_ebook$page/"
 echo "url_ebook=" $url_ebook
 url_for_download=$(curl $url_ebook | grep -o "http:\/\/filepi.*'" | rev | cut -c 2- | rev)
-echo "FIrst curl works? url_for_download=" $url_for_download
+echo "url_for_download=" $url_for_download
+real_url=$(curl $url_for_download -I -H  'Referer:'$url_ebook | grep -o "http[a-zA-Z0-9\/:.]*")
+echo "url_for_download=" $real_url
+echo
+wget --content-disposition -P /home/olena/Documents/Git-repositories/scripts/books $real_url
+let page=page+1
+fail_attempt=0
 echo -n "Continue? [y/n]: "
 read
-if [ "$REPLY" = "y" ]; then                             # debug pause
+if [ "$REPLY" = "y" ]; then                             # continue question
 echo
 else break    
 fi
-real_url=$(curl $url_for_download -I -H  'Referer:'$url_ebook | grep -o "http[a-zA-Z0-9\/:.]*")
-echo "2curl works? url_for_download=" $real_url
-echo
-wget --content-disposition  $real_url
-let page=page+1
-fail_attempt=0
 done
